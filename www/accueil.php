0c17eb48559c190297900f3d52260573c4a09ab6
Mael <72595888+MaelGibelot@users.noreply.github.com>
2023-01-18 09:32:25 +0100

    Delete www directory

--- www/accueil.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'navbar.php';
-?>
-<html>
-<body>
-<h2 style="margin-top:10em;display: flex; justify-content: center">Module X32I100</h2>
-<p style="display: flex; justify-content: center">Site et séries réalisé par CHEVALET Romain & GIBELOT Maël dans la cadre du module X32I100</p>
-</body>
-</html>
