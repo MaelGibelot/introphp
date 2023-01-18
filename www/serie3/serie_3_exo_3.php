f5395f88eb26a85b3c5e7c0d824764ccbe57619f
Mael <72595888+MaelGibelot@users.noreply.github.com>
2023-01-18 09:29:15 +0100

    Update serie_3_exo_3.php

--- www/serie3/serie_3_exo_3.php
+++ www/serie3/serie_3_exo_3.php
@@ -17,7 +17,8 @@ $somme = 0;
 $cpt = 0;
 $service = "En attente";
 
-if ($_POST['tranche'] == 1 && ($_POST['codeserv'] == 'p' || $_POST['codeserv'] == 'P')) {
+
+if ( isset($_POST['tranche']) && isset($_POST['codeserv']) && $_POST['tranche'] == 1 && ($_POST['codeserv'] == 'p' || $_POST['codeserv'] == 'P')) {
     //echo "L'utilisateur a téléphoné en journée et possède le service premium";
     if ($_POST['duree'] > 75) {
         $cpt = $_POST['duree'] - 75;
@@ -27,34 +28,36 @@ if ($_POST['tranche'] == 1 && ($_POST['codeserv'] == 'p' || $_POST['codeserv'] =
     }
 }
 
-if ($_POST['codeserv'] == 'r' || $_POST['codeserv'] == 'R') {
-    //echo "L'utilisateur a téléphoné en journée et possède le service classique";
-    if ($_POST['duree'] > 50) {
-        $cpt = $_POST['duree'] - 50;
-        $somme = 10 + $cpt * 0.2;
-    } else {
-        $somme = 10;
+if (isset($_POST['codeserv'])){
+
+    if (isset($_POST['duree']) && $_POST['codeserv'] == 'r' || $_POST['codeserv'] == 'R') {
+        //echo "L'utilisateur a téléphoné en journée et possède le service classique";
+        if ($_POST['duree'] > 50) {
+            $cpt = $_POST['duree'] - 50;
+            $somme = 10 + $cpt * 0.2;
+        } else {
+            $somme = 10;
+        }
+    }
+
+    if (isset($_POST['duree']) && isset($_POST['tranche']) && $_POST['tranche'] == 2 && ($_POST['codeserv'] == 'p' || $_POST['codeserv'] == 'P')) {
+        //echo "L'utilisateur a téléphoné de nuit et possède le service premium";
+        if ($_POST['duree'] > 100) {
+            $cpt = $_POST['duree'] - 100;
+            $somme = 25 + $cpt * 0.05;
+        } else {
+            $somme = 25;
+        }
+    }
+
+    if ($_POST['codeserv'] == 'r' || $_POST['codeserv'] == 'R') {
+        $service = "classique";
+
+    }
+    if ($_POST['codeserv'] == 'p' || $_POST['codeserv'] == 'P') {
+        $service = "premium";
     }
 }
-
-if ($_POST['tranche'] == 2 && ($_POST['codeserv'] == 'p' || $_POST['codeserv'] == 'P')) {
-    //echo "L'utilisateur a téléphoné de nuit et possède le service premium";
-    if ($_POST['duree'] > 100) {
-        $cpt = $_POST['duree'] - 100;
-        $somme = 25 + $cpt * 0.05;
-    } else {
-        $somme = 25;
-    }
-}
-
-if ($_POST['codeserv'] == 'r' || $_POST['codeserv'] == 'R') {
-    $service = "classique";
-}
-
-if ($_POST['codeserv'] == 'p' || $_POST['codeserv'] == 'P') {
-    $service = "premium";
-}
-
 if (!empty($_POST)) {
     if ($service == "classique" || $service == "premium") {
         echo "Le client numéro " . $_POST['numcl'] . " qui possède le service " . $service . " a passé un appel pendant " . $_POST['duree'] . " min. Il vous doit la somme de : " . $somme . " euros.";
